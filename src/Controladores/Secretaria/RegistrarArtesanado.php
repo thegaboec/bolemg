50e4d4a6854be532299a5b1d3ade064f0be925bb
thegaboec <gabrielguerrero38@gmail.com>
2023-04-17 22:33:09 -0500

    SUBIDA DE IMAGEN ARTESANADO COMPLETADO

--- src/Controladores/Secretaria/RegistrarArtesanado.php
+++ src/Controladores/Secretaria/RegistrarArtesanado.php
@@ -7,18 +7,34 @@ use App\Modelos\Artesanados;
 class RegistrarArtesanado{
     private $artesanados;
 
-    public function __construct( Artesanados $artesanados)
+
+    public function __construct(
+
+        Artesanados $artesanados
+    )
     {
-        $this-> artesanados = $artesanados;
+        $this->artesanados=$artesanados;
+        
     }
-    public function registrarArt($variable){
+
+    
+    public function registrarArt($variables=[]){
         return [
             'titulo' => 'Registrar Artesanado',
             'template' => 'secretaria/registrarArtesanado.html.php',
-            'variable' => $variable
+            'variables'=> $variables
+            
         ];
     }
+
+
     public function nuevoArtesanados(){
+
+        if(empty($_FILES['url_foto']['name'])){
+            return $this->registrarArt(
+                ['error' => 'Error no ingreso ninguna imagen']
+            );
+        }
 
         $tmp = $_FILES['url_foto']['tmp_name'];
         $name = $_FILES['url_foto']['name'];
@@ -26,33 +42,36 @@ class RegistrarArtesanado{
         $DirecionImage = './assets/img/';
         $outputImage = $DirecionImage . $name;
 
-        if(move_uploaded_file($tmp,$outputImage)){ // mueve la imagen guardada en el espacio temporal hacia la carpeta img permanentemente
-            $jsonImage = file_get_contents($direccionJson);// traemos el archivo json que contien la url de la imagen del logo
-            $dataImage = json_decode($jsonImage,true);
-            $dataImage['img'] = ltrim($outputImage,'.'); // le damos la nueva direccion del logo
-            file_put_contents($direccionJson,json_encode($dataImage)); 
-            return $this->registrarArt(['success' => 'Se cambio correctamente el logotipo']);
-        }else{
-            return $this->registrarArt(['error' => 'Error ocurrio un proble al guardar la imagen intente nuevamente']);
-        }
-    
         $add =[
             'idartesanado' => trim($_POST['idartesanado']),
             'categoria' => trim($_POST['categoria']), 
-            'url_foto' => $DirecionImage, 
+            'url_foto' => trim($outputImage), 
             'estado' => Artesanados::ESTADO_ACTIVO
         ];
 
         try{
+
+            if(move_uploaded_file($tmp,$outputImage)){ // mueve la imagen guardada en el espacio temporal hacia la carpeta img permanentemente
+                $jsonImage = file_get_contents($direccionJson);// traemos el archivo json que contien la url de la imagen del logo
+                $dataImage = json_decode($jsonImage,true);
+                $dataImage['img'] = ltrim($outputImage,'.'); // le damos la nueva direccion del logo
+                file_put_contents($direccionJson,json_encode($dataImage)); 
+    
+            }
+
             $this ->artesanados->insert($add);
-            return[
-                'titulo' => 'Registrar Artesanado',
-                'template' => 'secretaria/registrarArtesanado.html.php',
-                'variables' => [
-                        
-                    'success' => 'Se registro correctamente el nuevo artesanado'
-                ]
-                ];
+            
+
+                return[
+                    'titulo' => 'Registrar Artesanado',
+                    'template' => 'secretaria/registrarArtesanado.html.php',
+                    'variables' => [
+                            
+                        'success' => 'Se registro correctamente el nuevo artesanado'
+                    ]
+                    ];
+                
+              
             
         }catch(\PDOException $e){
                 return [
@@ -64,5 +83,12 @@ class RegistrarArtesanado{
                         ] 
                      ];
         }
+
+        
+    
+        
+
+        
     }
+
 }
