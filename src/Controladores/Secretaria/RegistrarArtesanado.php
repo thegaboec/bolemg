f6b0ab60cd8c7fc7dda5dfe8e8d4be5c9b07e990
CristinaCaluna <calunacristina9@gmail.com>
2023-04-17 20:34:44 -0500

    cambio

--- src/Controladores/Secretaria/RegistrarArtesanado.php
+++ src/Controladores/Secretaria/RegistrarArtesanado.php
@@ -18,13 +18,14 @@ class RegistrarArtesanado{
             'variable' => $variable
         ];
     }
-    public function nuevoArtesanados(){
 
+       public function nuevoArtesanados (){
         $tmp = $_FILES['url_foto']['tmp_name'];
         $name = $_FILES['url_foto']['name'];
         $direccionJson = './src/Includes/imgartesanado.json';
         $DirecionImage = './assets/img/';
         $outputImage = $DirecionImage . $name;
+        
 
         if(move_uploaded_file($tmp,$outputImage)){ // mueve la imagen guardada en el espacio temporal hacia la carpeta img permanentemente
             $jsonImage = file_get_contents($direccionJson);// traemos el archivo json que contien la url de la imagen del logo
@@ -35,11 +36,14 @@ class RegistrarArtesanado{
         }else{
             return $this->registrarArt(['error' => 'Error ocurrio un proble al guardar la imagen intente nuevamente']);
         }
-    
+
+        
+
+ 
         $add =[
             'idartesanado' => trim($_POST['idartesanado']),
             'categoria' => trim($_POST['categoria']), 
-            'url_foto' => $DirecionImage, 
+            'url_foto' =>  $outputImage,
             'estado' => Artesanados::ESTADO_ACTIVO
         ];
 
